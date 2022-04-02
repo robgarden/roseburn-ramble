20f93ce33e828462749f6f97dd6c58607559b0d1
Robert Garden <robert.garden@valr.com>
2022-04-02 22:17:14 +0200

    feat: donation page

--- src/pages/Donation.tsx
+++ src/pages/Donation.tsx
@@ -1,10 +1,28 @@
-import { Container, Heading, MainContainer } from "../styles";
+import {
+  Container,
+  Heading,
+  MainContainer,
+  Paragraph,
+  StyledLink,
+} from "../styles";
+
+const DONATION_LINK = "https://www.peoplesfundraising.com/donation/playground";
 
 export function DonationPage() {
   return (
     <MainContainer>
       <Container>
-        <Heading>While you're here</Heading>
+        <Heading>One more thing</Heading>
+        <Paragraph>As you can see, there's no playground in sight</Paragraph>
+        <Paragraph>An extra donation would help with our plight!</Paragraph>
+        <img
+          src="/images/playground.jpeg"
+          alt="playground"
+          style={{ width: "100%", borderRadius: "2px", marginTop: "30px" }}
+        />
+        <StyledLink as={"a"} href={DONATION_LINK} target="_blank" style={{ marginTop: "50px"}}>
+          Donate
+        </StyledLink>
       </Container>
     </MainContainer>
   );
